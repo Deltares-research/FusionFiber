5b9e71d43dd2bf62dc596b0a663fd51b2654690a
Roeland Nieboer <roelandnieboer@gmail.com>
2026-03-16 18:29:32 +0100

    adde pumprate to darcy tool

---
+++ tools/pumprate_to_darcyflux.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def darcy_flux_m_per_day(flow_rate_m3_per_day, well_screen_height_m, well_screen_radius_m, distance_from_screen_m):
+    """
+    Radial Darcy flux q(r) around a well, returned in m/day.
+
+    Parameters
+    ----------
+    flow_rate_m3_per_day : float
+        Pumping or injection rate [m^3/day]
+    well_screen_height_m : float
+        Well screen length [m]
+    well_screen_radius_m : float
+        Well screen radius [m]
+    distance_from_screen_m : float or array
+        Distance from well screen surface [m]
+
+    Returns
+    -------
+    q_day : float or array
+        Darcy flux at distance from well screen surface [m/day]
+    """
+    distance_from_screen_m = np.asarray(distance_from_screen_m)
+    radial_distance_from_center_m = well_screen_radius_m + distance_from_screen_m
+    cylindrical_flow_area_m2 = 2 * np.pi * radial_distance_from_center_m * well_screen_height_m
+    q_day = flow_rate_m3_per_day / cylindrical_flow_area_m2
+    return q_day
+
+# Example
+pumping_rate_m3_per_day = 1.5*24          # m3/day
+well_screen_height_m = 1.1                 # m
+well_screen_radius_m = 0.1           # m
+distance_from_screen_m = [0, 0.05, 0.1]  # m
+
+print(darcy_flux_m_per_day(
+    pumping_rate_m3_per_day,
+    well_screen_height_m,
+    well_screen_radius_m,
+    distance_from_screen_m,
+))
